09285ed5b4de229fcbc49c35a3f84f5e9620977c
Tom Close <tom.close@cantab.net>
2012-11-26 18:17:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McrBar::Application.config.secret_token = '550b2af5884f44792ead941d4ff4e133eec653f661380613269b49687f98c1c1633e5003e8c414242f5adfb71d1eaa3e58405f49851a667718ee64a219467796'
